58a28bd5be1b7dda6520b532fbb17a3b50831c4d
AdSchl2E <89904512+AdSchl2E@users.noreply.github.com>
2024-11-27 17:31:59 +0100

    Finale

--- src/main/java/util/GeoNetApi.java
+++ src/main/java/util/GeoNetApi.java
@@ -50,7 +50,7 @@ Exemple d'utilisation:
 
 public class GeoNetApi {
 
-    private static final String MA_CLE_GOOGLE_API = "AIzaSyDMqXyR2llzWs1F_K147AwbfA89XouummE"; // (DASI-0x04)
+    private static final String MA_CLE_GOOGLE_API = ""; // (DASI-0x04) A COMPLETER
 
     private static final GeoApiContext MON_CONTEXTE_GEOAPI = new GeoApiContext.Builder().apiKey(MA_CLE_GOOGLE_API).build();
 
